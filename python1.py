f1743a0c0e5f728e4205e078d36a2eb44f011cc8
rudra <rpsingh98188@gmail.com>
2023-09-10 20:30:10 +0530

    first commit

---
+++ python1.py
@@ -0,0 +1,8 @@
+num=int(input("enter the year :-"))
+if num%400==0 and num%100==0:
+    print(num,"is a leap year")
+elif (num%4==0) and (num%4==0):
+    print(num,"is a leap year")
+else:
+    print(num,"is not a leap year ")
+    
